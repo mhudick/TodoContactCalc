9f2f4bd574ea0c686a30ae2c3d66533ad3a538e5
mhudick <m.hudick@ent0.com>
2014-08-22 13:14:25 -0400

    validates

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,2 +1,11 @@
 class TodoList < ActiveRecord::Base
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :address, presence: true
+  validates :phone_number, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
+
+
 end
--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -5,5 +5,9 @@ describe 'creating todo lists' do
     visit '/todo_lists'
     click_link "New Todo list"
     expect(page).to have_content("New To do list")
+    fill_in "Title", with: "My Todo list"
+    fill_in "Description", with: "a fun thing"
+    click_link "Create Todo list"
+    expect(page).to have_content("Todo list was successfully created.")
   end
 end
